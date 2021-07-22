c782c8a88df6c31c1f1ea81f63af9b757ec42f23
luliangyu <luliangyu@seewintech.com>
2021-07-22 10:43:48 +0800

    管道模式

---
+++ pipeline-pattern/src/main/java/com/byaoh/pipeline/App.java
@@ -0,0 +1,17 @@
+package com.byaoh.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * App
+ *
+ * @author luliangyu
+ * @date 2021-07-22 10:26
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
---
+++ pipeline-pattern/src/main/java/com/byaoh/pipeline/route/RouteFactory.java
@@ -0,0 +1,13 @@
+package com.byaoh.pipeline.route;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RouteFactory
+ *
+ * @author luliangyu
+ * @date 2021-07-22 09:37
+ */
+@Configuration
+public class RouteFactory {
+}
